c4cbd6e82e06d992f4000812506d8b04b4dc5c56
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-08 09:54:10 -0500

    Introduction page complete.

---
+++ Exercises.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+/*:
+ # Introduction
+ 
+ The purpose of this playground is to check your knowledge of concepts from these sections of *App Development with Swift*:
+ 
+ * 1.2: Constants, Variables, and Data Types
+ * 1.3: Operators
+ * 1.4: Control Flow
+ 
+ You will receive feedback from Mr. Gordon, via GitHub, on your work in this playground after you commit it and add links to your Computer Science portfolio.
+ 
+ The next step (after completing this playground) is to elect, in a class prior the Winter Break, to complete a similar playground (entirely in class) that will further demonstrate what you have learned.
+ 
+ Ready? Let's begin.
+ 
+ */
+//: [Next](@next)
