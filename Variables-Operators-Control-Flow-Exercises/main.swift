d9cf9ff9a0b5c3901a1a5cb147b94a1b7a2bc8b9
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-08 09:46:46 -0500

    Initial Commit

---
+++ Variables-Operators-Control-Flow-Exercises/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Variables-Operators-Control-Flow-Exercises
+//
+//  Created by Gordon, Russell on 2017-12-08.
+//  Copyright © 2017 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
