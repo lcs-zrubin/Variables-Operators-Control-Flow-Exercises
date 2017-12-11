450d53f596cf16176686ab0fac26b065c0d18bf7
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-11 13:30:25 -0500

    Finished questions based on 1.2

--- Exercises.playground/Pages/Constants, Variables, Data Types.xcplaygroundpage/Contents.swift
+++ Exercises.playground/Pages/Constants, Variables, Data Types.xcplaygroundpage/Contents.swift
@@ -7,8 +7,9 @@
  Create and initialize a variable or constant (whatever is more appropriate) to represent each of these items:
  
  * Student first name
+ * Student last name
  * Student's current grade
- * Student's final grade for the year
+ * Student's grade for the November report card
  
  */
 // Answer Question 1 below
@@ -16,11 +17,11 @@
 /*:
  ### Question 2
  
- Create a constant called `myAge` of type `Int` and set it to your age. Use inference to do this.
+ Create a constant called `myAge` of type `Int` and set it to your age. Use type inference to do this.
  
  Then create a constant called `otherAge` of type `Double` and set it to `40.0`. Use type inference to do this.
  
- Finally, create a constant called `averageAge` and set it to the average of `myAge` and `otherAge`. Use type annotation to do this.
+ Finally, create a constant called `averageAge` of type `Double` and set it to the average of `myAge` and `otherAge`.
  
  */
 // Answer Question 2 below
@@ -30,11 +31,50 @@
  ### Question 3
  
  Create a variable that will track a person's favourite hobby. Select whatever name you feel is appropriate, but use the camel case convention when you name this constant.
+ */
+// Answer Question 3 below
+
+
+/*:
+ ### Question 4
  
- Create a constant that will track the 
+ What are the advantages of using constants, which are *immutable*?
+ 
+ Use multiple `print` statements below to answer this question.
+ */
+// Answer Question 4 below
+
+
+/*:
+ ### Question 5
+ 
+ ![million](million.jpg "I demand the sum of one million dollars!")
+ 
+ Define a constant named `randomDemand` and set the value to one million dollars, using underscores to make the value more readable.
  
  */
-// Answer Question 2 below
+// Answer Question 5 below
+
+/*:
+ ### Question 6
+ 
+ Declare a variable and set it to 2017.
+ 
+ Then declare a constant and initialize it with a short piece of text.
+ 
+ Then, try to assign the value of the constant to the value of the variable.
+ 
+ Does this work?
+ 
+ If not, comment out the code that causes the error.
+ 
+ Then, use multiple `print` statements below to explain why the code does not work.
+
+ */
+// Answer Question 6 below
+
+
+
 
 
 
