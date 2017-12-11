bd242f8fe513256aded72c34934d9896582707fd
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-11 13:09:12 -0500

    Question 3 completed

---
+++ Exercises.playground/Pages/Constants, Variables, Data Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,41 @@
+//: [Previous](@previous)
+/*:
+ # Constants, Variables, Data Types
+ 
+ ### Question 1
+ 
+ Create and initialize a variable or constant (whatever is more appropriate) to represent each of these items:
+ 
+ * Student first name
+ * Student's current grade
+ * Student's final grade for the year
+ 
+ */
+// Answer Question 1 below
+
+/*:
+ ### Question 2
+ 
+ Create a constant called `myAge` of type `Int` and set it to your age. Use inference to do this.
+ 
+ Then create a constant called `otherAge` of type `Double` and set it to `40.0`. Use type inference to do this.
+ 
+ Finally, create a constant called `averageAge` and set it to the average of `myAge` and `otherAge`. Use type annotation to do this.
+ 
+ */
+// Answer Question 2 below
+
+
+/*:
+ ### Question 3
+ 
+ Create a variable that will track a person's favourite hobby. Select whatever name you feel is appropriate, but use the camel case convention when you name this constant.
+ 
+ Create a constant that will track the 
+ 
+ */
+// Answer Question 2 below
+
+
+
+//: [Next](@next)
