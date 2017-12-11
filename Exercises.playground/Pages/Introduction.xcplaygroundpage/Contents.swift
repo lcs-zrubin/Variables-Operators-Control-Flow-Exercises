90200958a803b75e5a5167b11f5e56ca4de10346
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-11 14:12:08 -0500

    Changed introduction text.

--- Exercises.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Exercises.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -5,11 +5,16 @@
  
  * 1.2: Constants, Variables, and Data Types
  * 1.3: Operators
- * 1.4: Control Flow
  
  You will receive feedback from Mr. Gordon, via GitHub, on your work in this playground after you commit it and add links to your Computer Science portfolio.
  
- In our very next class together, you will write another set of exercises like this.  Links to your code must go into your portfolio; they are there to help you demonstrate your mastery of course expectations.
+ In our very next class together on Thursday, you will write another set of exercises like this to check your knowledge of content from section 1.4 (Control Flow).
+ 
+ After you add links to your portfolio from Thursday's class, you will also receive feedback on your work.
+ 
+ Finally, on Saturday, you will write a third playground that again covers content from 1.2, 1.3, and 1.4.
+ 
+ This playground for Saturday's class serves as another layer of documentation of your learning for these key topics.
  
  Remember, the more documentation of learning that you have, the more you can expect and justify a top-level grade in this course.
  
