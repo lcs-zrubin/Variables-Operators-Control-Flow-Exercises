3778ff17577a5b20a42b2366a8366414c3318ef6
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-12 12:42:55 -0500

    if and else-if questions done

--- Exercises.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ Exercises.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -27,9 +27,47 @@
  
  ### Question 1
  
- You may not know this but there is much money to be made babysitting younger children. Certain faculty at LCS would really love to find a responsible teenager to watch their young ones from time to time.
+ You've noticed that your parents seem attached to watching weather reports on the television for some reason, rather than just checking the Weater app on their phone.
+ 
+ You've also noticed that the weather reporter's comments are very predictable.
+ 
+ A variable `temperature` has been created below with a value of 0.
+
+ Write an if statement that prints "Brr, grab your winter jacket!" when the temperature is equal to 0.
+ */
+var temperature = 0
+
+// Answer Question 1 below
+
+
+/*:
+ 
+ ### Question 2
+ 
+ `temperature` has been updated below to be -20.
+ 
+ Write an if-else if statement that prints "Brr, grab your winter jacket!" if `temperature` is equal to 0, and temperature is anything less than 0, prints "Don't leave your water glass outside!".
+ */
+temperature = -20
+
+// Answer Question 2 below
+
+
+/*:
+ 
+ ### Question 3
+ 
+ `temperature` has been updated below to be 15 degrees.
+ 
+ Write an if-else if-else statement that prints:
+ 
+ * "Brr, grab your winter jacket!" if `temperature` is equal to 0
+ * "Don't leave your water glass outside!" if `temperature` is anything less than 0
+ * Otherwise, prints "It could be shorts weather but you might still be a bit chilly!"
  
  */
-// Answer Question 1 below
+temperature = 15
+
+// Answer Question 3 below
 
 
