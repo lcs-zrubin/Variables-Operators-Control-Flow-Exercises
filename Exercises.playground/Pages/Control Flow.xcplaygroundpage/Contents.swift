2dbbfa8a8fb1fb22aa27aab772f0a0e42f1244fd
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-12 12:18:03 -0500

    Added control flow page and learning goals for this page.

---
+++ Exercises.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+/*:
+ # Control Flow
+ 
+ ## Learning Goals - Programming
+ 
+ * Goal 5
+    * *Knowledge*
+        * I can write concise statements using Boolean operators or pattern matching to check for multiple conditions.
+ * Goal 6
+    * *Knowledge*
+        * I can use comparison operators and arithmetic operators and understand operator precedence (order of operations).
+ 
+ * Goal 8
+    * *Knowledge*
+        * I can use sequence, selection, and repetition control structures to write programs that accept user input, perform processing tasks, and produce output, with user-friendly interfaces.
+ 
+ ## Learning Goals - Software Development
+ 
+ * Goal 8
+    * *Communication*
+        * I can write human-readable programs (naming, indenting) with appropriate documentation (comments).
+ 
+ * Goal 9
+    * *Communication*
+        * I can effectively use the file manager of my operating system (Finder) and source control tools (git, GitHub) to organize and back up my work.
+ 
+ ### Question 1
+ 
+ You may not know this but there is much money to be made babysitting younger children. Certain faculty at LCS would really love to find a responsible teenager to watch their young ones from time to time.
+ 
+ */
+// Answer Question 1 below
+
+
